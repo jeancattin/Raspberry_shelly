5ea0f59f16f7919f4475522432d09df82dc24b01
jeancattin <jean.cattin@protonmail.ch>
2023-09-17 00:28:30 +0200

    Add extra measurements

--- ShellyCheck_influx.py
+++ ShellyCheck_influx.py
@@ -12,6 +12,11 @@ INFLUXDB_DATABASE = 'shelly1'  # Replace with the desired database name
 SHELLY_3EM_IP = "http://192.168.0.11"
 SHELLY_1PM_IP = "192.168.0.12"
 
+# Constants for Time Intervals
+DATA_INTERVAL = timedelta(seconds=10)
+SLEEP_INTERVAL = 1  # Adjust the time interval based on your requirements
+
+
 def write_to_influxdb(client, measurement, meas_time, tags, fields):
     json_body = [
         {
@@ -34,7 +39,7 @@ def get_3em_data(port):
         print(f"Failed to retrieve 3em data from port {port}. Status code: {response.status_code}")
         return None
     
-def get_balcon_data():
+def get_1pm_data():
     response = requests.get(f"http://{SHELLY_1PM_IP}/rpc/Switch.GetStatus?id=0", timeout=5)
     if response.status_code == 200:
         data = response.json()
@@ -42,6 +47,8 @@ def get_balcon_data():
     else:
         print(f"Failed to retrieve balcony data. Status code: {response.status_code}")
         return None
+    
+
 
 def main():
     # Connect to InfluxDB
@@ -49,59 +56,86 @@ def main():
                             database=INFLUXDB_DATABASE)
     
     # Initialize variables for mean power calculation
-    start_time = datetime.utcnow()
-    power_accumulated_3em = [0] * 3
-    power_accumulated_1pm = 0
+    mean_accumulated = {
+        "3em": {
+            "power": [0] * 3,
+            "voltage": [0] * 3,
+            "current": [0] * 3,
+            "pf": [0] * 3
+        },
+        "1pm": {
+            "power": 0,
+            "voltage": 0,
+            "current": 0
+        }
+    }
+    
     samples_count = 0
-
+    start_time = datetime.utcnow()
+    
     while True:
         # Read data from Shelly 3EM ports and accumulate power
-        for port in range(3):
+        for port in range(3): 
             shelly_3em_data = get_3em_data(port=port)
             if shelly_3em_data:
-                power_accumulated_3em[port] += shelly_3em_data["power"]
-        
+                mean_accumulated["3em"]["power"][port] += shelly_3em_data["power"]
+                mean_accumulated["3em"]["voltage"][port] += shelly_3em_data["voltage"]
+                mean_accumulated["3em"]["current"][port] += shelly_3em_data["current"]
+                mean_accumulated["3em"]["pf"][port] += shelly_3em_data["pf"]
+
         # Read data from Shelly Plus 1PM and accumulate power
-        shelly_1pm_data = get_balcon_data()
+        shelly_1pm_data = get_1pm_data()
         if shelly_1pm_data:
-            power_accumulated_1pm += shelly_1pm_data["apower"]
-        
+            mean_accumulated["1pm"]["power"] += shelly_1pm_data["apower"]
+            mean_accumulated["1pm"]["voltage"] += shelly_1pm_data["voltage"]
+            mean_accumulated["1pm"]["current"] += shelly_1pm_data["current"]
+
         samples_count += 1
 
-        # Check if 10 seconds have passed
+        # Check if the timeinterval has passed
         current_time = datetime.utcnow()
         time_difference = current_time - start_time
-        if time_difference >= timedelta(seconds=5):
-            # Calculate mean power for each channel
-            mean_power_3em = [power / samples_count for power in power_accumulated_3em]
-            mean_power_1pm = power_accumulated_1pm / samples_count
-            
-            # Write mean power to InfluxDB
+        if time_difference >= DATA_INTERVAL:
+            # Calculate mean measurements for each channel
             for port in range(3):
                 measurement = "shelly_3em"
                 tags = {
                     "port": port
                 }
                 fields = {
-                    "power": mean_power_3em[port]
+                    "power": mean_accumulated["3em"]["power"][port] / samples_count,
+                    "voltage": mean_accumulated["3em"]["voltage"][port] / samples_count,
+                    "current": mean_accumulated["3em"]["current"][port] / samples_count,
+                    "pf": mean_accumulated["3em"]["pf"][port] / samples_count
                 }
                 write_to_influxdb(client, measurement, current_time, tags, fields)
+  
             measurement = "shelly_1pm"
             tags = {
                 "tag": "NA"
             }
             fields = {
-                "power": mean_power_1pm
+                "power": mean_accumulated["1pm"]["power"] / samples_count,
+                "voltage": mean_accumulated["1pm"]["voltage"] / samples_count,
+                "current": mean_accumulated["1pm"]["current"] / samples_count,
             }
             write_to_influxdb(client, measurement, current_time, tags, fields)
-            
-            # Reset variables for the next 10-second interval
-            power_accumulated_3em = [0] * 3
-            power_accumulated_1pm = 0
-            samples_count = 0
-            start_time = current_time
 
-        time.sleep(1)  # Adjust the time interval based on your requirements
+            # Reset variables for the next 10-second interval
+            for key in mean_accumulated:
+                if isinstance(mean_accumulated[key], dict):
+                    for subkey in mean_accumulated[key]:
+                        if isinstance(mean_accumulated[key][subkey], list):
+                            mean_accumulated[key][subkey] = [0] * len(mean_accumulated[key][subkey])
+                        else:
+                            mean_accumulated[key][subkey] = 0
+                else:
+                    mean_accumulated[key] = 0
+            samples_count = 0
+            start_time = datetime.utcnow()
+
+
+        time.sleep(SLEEP_INTERVAL)  # Adjust the time interval based on your requirements
 
 if __name__ == "__main__":
     main()
